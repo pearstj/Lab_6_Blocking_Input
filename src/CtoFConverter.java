ed6ad73051d338b7fa977e7eb46a05c34c608ceb
pearstj <pearstj@mail.uc.edu>
2025-09-29 21:48:41 -0400

    ctof

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CtoFConverter {
+class CtoFConverter {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         double celsius = 0;
