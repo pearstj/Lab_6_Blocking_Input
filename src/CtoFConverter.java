774f39dfd17fcc2612d324954e370c375c2f3da7
pearstj <pearstj@mail.uc.edu>
2025-09-29 21:28:00 -0400

    Initial commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CtoFConverter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double celsius = 0;
+        String trash = "";
+        boolean done = false;
+
+        // Input loop for Celsius temperature
+        do {
+            System.out.print("Enter temperature in Celsius: ");
+
+            if(in.hasNextDouble()) {
+                celsius = in.nextDouble();
+                in.nextLine(); // clear the newline from buffer
+                done = true;
+            }
+            else {
+                trash = in.nextLine();
+                System.out.println("\nYou entered: " + trash);
+                System.out.println("You must enter a valid number!");
+            }
+        } while(!done);
+
+        // Convert Celsius to Fahrenheit: F = (C * 9/5) + 32
+        double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+        // Display result
+        System.out.println("\n" + celsius + " degrees Celsius = " + fahrenheit + " degrees Fahrenheit");
+
+        in.close();
+    }
+}
